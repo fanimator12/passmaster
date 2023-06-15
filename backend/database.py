f373d117facd493893b89cb50136d416b1ed489f
Franciska Török <torok.franciska.leonora@gmail.com>
2023-06-15 19:24:49 +0200

    FastAPI successfully deployed on Kubernetes

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:fanimator@localhost:5432/passmaster_db"
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:fanimator@passmaster-db:5432/passmaster_db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 Base = declarative_base()
