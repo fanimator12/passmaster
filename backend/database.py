f23a26d00d73b637b8191a97828b22d76d57ad10
Franciska Török <torok.franciska.leonora@gmail.com>
2023-06-15 23:22:51 +0200

    modify cloud db

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:fanimator@passmaster-db:5432/passmaster_db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:fanimator@34.88.44.110:5432/passmaster'
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 Base = declarative_base()
 SessionLocal=sessionmaker(bind = engine)
+
+try:
+    with engine.connect() as connection:
+        result = connection.execute('SELECT 1')
+        print(result.fetchone())
+        print('Connection successful!')
+except Exception as e:
+    print('Connection failed:', str(e))
